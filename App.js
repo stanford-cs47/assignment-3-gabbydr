f249abbf26413ba968c4fed8092c78296136b0b3
gabbydr <gabbyr@stanford.edu>
2019-10-18 20:06:53 -0700

    Initial push

--- App.js
+++ App.js
@@ -8,10 +8,11 @@
 */
 
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Dimensions,
+  TextInput, Image, TouchableOpacity, Keyboard, FlatList } from 'react-native';
 import { Images, Colors } from './App/Themes'
 import APIRequest from './App/Config/APIRequest'
-
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 import News from './App/Components/News'
 import Search from './App/Components/Search'
 
@@ -25,9 +26,8 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-
     //uncomment this to run an API query!
-    //this.loadArticles();
+    this.loadArticles();
   }
 
   async loadArticles(searchTerm = '', category = '') {
@@ -42,34 +42,133 @@ export default class App extends React.Component {
     this.setState({loading: false, articles: resultArticles})
   }
 
+  onChangeText = text => {
+    this.setState({searchText: text});
+  }
+
+  searchArticles = () => {
+    var articles = this.loadArticles(this.state.searchText);
+    console.log("Article Results: ", articles);
+  }
+
+  renderArticles = (item) => (
+    <News
+      title={item.title}
+      content={item.snippet}
+      author={item.byline}
+      url={item.url}
+      />
+  )
+
+  keyExtractor = index => {
+    return index.toString();
+  }
+
   render() {
     const {articles, loading} = this.state;
 
     return (
-      <SafeAreaView style={styles.container}>
+        <SafeAreaView style={styles.container}>
 
-        <Text style={{textAlign: 'center'}}>Have fun! :) {"\n"} Start by changing the API Key in "./App/Config/AppConfig.js" {"\n"} Then, take a look at the following components: {"\n"} NavigationButtons {"\n"} Search {"\n"} News {"\n"} 🔥</Text>
+          <View style={styles.titleBar}>
+            <Image style={styles.logo}
+              source={Images.logo}/>
+          </View>
 
-        {/*First, you'll need a logo*/}
+          <View style={styles.searchBarContainer}>
 
-        {/*Then your search bar*/}
+              <TextInput
+                placeholder = "Search for News"
+                style={styles.searchBar}
+                onChangeText={text => this.onChangeText(text)}
+                value={this.state.searchText}
+                onSubmitEditing={() => this.searchArticles()}
+                />
 
-        {/*And some news*/}
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={this.searchArticles}
+                  >
+                  <AntDesign name = "search1"
+                    size={25}
+                    color="red"
+                    />
+                </TouchableOpacity>
+          </View>
 
-        {/*Though, you can style and organize these however you want! power to you 😎*/}
+          <News articles={this.state.articles}/>
 
-        {/*If you want to return custom stuff from the NYT API, checkout the APIRequest file!*/}
+          {/*And some news*/}
 
-      </SafeAreaView>
-    );
+          {/*Though, you can style and organize these however you want! power to you 😎*/}
+
+          {/*If you want to return custom stuff from the NYT API, checkout the APIRequest file!*/}
+
+        </SafeAreaView>
+      );
   }
 }
 
+const { height, width } = Dimensions.get('window')
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'column',
+    alignContent: 'center',
+  },
+  titleBar: {
+    height: height * .09,
+    width: width,
+    paddingTop: 15,
+    paddingBottom: 15,
+    alignItems: 'center',
+    justifyContent: 'center'
+
+  },
+  searchBarContainer: {
+    flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    width: width * .8,
+    height: 40,
+  },
+  flatList: {
+    height: height * .8,
+    width: width,
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    alignContent: 'flex-start'
+  },
+  logo: {
+    height: height * .07,
+    width: width * .92,
+  },
+  searchBar: {
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderWidth: 2,
+    width: width * .8,
+    height: 40,
+    backgroundColor: '#ededed',
+    borderColor: '#ededed',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+  search: {
+    tintColor: 'red',
+    height: 15,
+    width: 15,
+    justifyContent: 'flex-end',
+  },
+  button: {
+    backgroundColor: '#ededed',
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    borderWidth: 2,
+    borderColor: '#ededed',
+    height: 40,
   }
 });
--- App/Components/News.js
+++ App/Components/News.js
@@ -9,7 +9,7 @@
 
 import React, { Component } from 'react'
 import PropTypes from 'prop-types' //consider using this!
-import { StyleSheet, SafeAreaView, View, FlatList, Text, Linking } from 'react-native'
+import { StyleSheet, SafeAreaView, View, FlatList, Text, Linking, Dimensions } from 'react-native'
 import { material } from 'react-native-typography' //consider using this!
 import { Metrics, Colors } from '../Themes'
 
@@ -20,6 +20,15 @@ export default class News extends Component {
     articles: PropTypes.array
   }
 
+  renderArticles = (item) => (
+    <News
+      title={item.title}
+      content={item.snippet}
+      author={item.byline}
+      url={item.url}
+      />
+  )
+
   //you can change the props above to whatever you want/need.
 
   render () {
@@ -27,15 +36,38 @@ export default class News extends Component {
 
     return (
       <View style={styles.container}>
-        {/*Some FlatList or SectionList*/}
+        <FlatList
+          data={articles}
+          renderItem={({ item }) => (
+            <View style = {styles.article}>
+              <Text style={material.headline}>{item.title}</Text>
+              <Text style={material.body1}>{item.snippet}</Text>
+              <Text style={material.body2}>{item.byline}</Text>
+              <Text style={material.caption}>{item.date}</Text>
+              <Text style={material.caption}>{item.url}</Text>
+            </View>
+          )}
+          keyExtractor={item => item.url}
+          >
+          </FlatList>
       </View>
     );
   }
 }
 
-
+const { height, width } = Dimensions.get('window')
 const styles = StyleSheet.create({
   container: {
-
+    height: height * .8,
+    width: width,
+    paddingTop: 30
   },
+  article: {
+    height: height * .3,
+    width: width,
+    padding: 15
+  },
+  title: {
+    fontSize: 25
+  }
 });
